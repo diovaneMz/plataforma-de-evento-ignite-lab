126d83055f454e721861b837420f721a128b8bf3
diovaneMz <diovanemarcelinoazevedo@gmail.com>
2022-06-21 18:46:45 -0300

    remove apollo.ts

--- src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ojf7z908hx01xxeb5f22y3/master',
-  cache: new InMemoryCache()
-})
