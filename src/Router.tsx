c03c99744136a30ad91c7ff8ea7a1e5c39a7b24b
diovaneMz <diovanemarcelinoazevedo@gmail.com>
2022-06-28 10:02:29 -0300

    click is blocked if the class is not available

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,6 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Subscribe />} />
-      <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
   );
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -22,7 +22,7 @@ export function Lesson(props: LessonProps) {
   const isActiveLesson = slug === props.slug
 
   return (
-    <Link to={`/event/lesson/${props.slug}`} className="group">
+    <Link to={isLessonAvailable ? `/event/lesson/${props.slug}` : ''} className="group">
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
