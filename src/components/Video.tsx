3e8499cd3d46b3721b126f0b2e54e4f1a1fdcc6e
diovaneMz <diovanemarcelinoazevedo@gmail.com>
2022-06-28 13:05:20 -0300

    favicon add

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -86,7 +86,8 @@ export function Video(props: VideoProps) {
             </a>
 
             <a
-              href=""
+              onClick={() => alert('Sem desafios disponiveis pra essa aula')}
+              href="#"
               className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors w-full lg:max-w-xs"
             >
               <Lightning size={24} />
