a4202c1f98ef93c549a67afb04e2597769ccda33
diovaneMz <diovanemarcelinoazevedo@gmail.com>
2022-06-28 11:09:27 -0300

    subscribe screen responsive

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -40,23 +40,23 @@ export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="w-full max-w-[1100px] flex flex-col items-center mt-10 mx-auto lg:flex-row lg:justify-between lg:mt-20">
-        <div className="max-w-[640px]  text-center lg:text-left">
-          <div className="w-full flex justify-center items-center">
+        <div className="max-w-[640px] text-center lg:text-left">
+          <div className="w-full flex justify-center items-center lg:justify-start">
             <Logo />
           </div>
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight ">
+          <h1 className="mt-8 text-[2.5rem] leading-tight lg:mt-4 ">
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do
             zero, com <strong className="text-blue-500">React</strong>
           </h1>
-          <p className="mt-8 text-gray-200 leading-relaxed">
+          <p className="mt-8 text-gray-200 leading-relaxed lg:mt-4">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais
             utilizadas e com alta demanda para acessar as melhores oportunidades do
             mercado.
           </p>
         </div>
 
-        <div className="p-8 mt-8 bg-gray-700 border border-gray-500 rounded w-full lg:max-w-sm">
+        <div className="p-8 mt-8 bg-gray-700 border border-gray-500 rounded-none w-full lg:max-w-sm lg:rounded ">
           <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
