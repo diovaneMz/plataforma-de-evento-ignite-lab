24b6dac636ff39640ec6e7091a70ecdbe6ff7c2d
diovaneMz <diovanemarcelinoazevedo@gmail.com>
2022-06-28 11:17:17 -0300

    video area responsive

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -44,7 +44,7 @@ export function Video(props: VideoProps) {
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
-        <div className="flex items-start gap-16">
+        <div className="flex flex-col items-start gap-16 lg:flex-row">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200">{data.lesson.description}</p>
@@ -69,11 +69,11 @@ export function Video(props: VideoProps) {
             )}
           </div>
 
-          <div className="flex flex-col gap-4 ">
+          <div className="flex flex-col gap-4 w-full lg:max-w-xs">
             <a
               href="https://discord.gg/txPXAZgFbd"
               target='_blank'
-              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-color "
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-color w-full lg:max-w-xs"
             >
               <DiscordLogo size={24} />
               Comunidade do Discord
@@ -81,7 +81,7 @@ export function Video(props: VideoProps) {
 
             <a
               href=""
-              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors w-full lg:max-w-xs"
             >
               <Lightning size={24} />
               Acesse o desafio
@@ -89,7 +89,7 @@ export function Video(props: VideoProps) {
           </div>
         </div>
 
-        <div className="gap-8 mt-20 grid grid-cols-2">
+        <div className="gap-8 mt-20 grid grid-rows-2 lg:grid-cols-2">
           <a
             href="#"
             className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-color"
